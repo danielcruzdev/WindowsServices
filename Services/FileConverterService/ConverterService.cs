a17def62f614bc268c6dbbc82000196ce205cc46
Daniel Cruz <daniel.cruz@writesys.com.br>
2020-07-21 15:42:46 -0300

    Deploy a simple service to edit a file

---
+++ Services/FileConverterService/ConverterService.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace FileConverterService
+{
+    class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"C:\temp\", "file.txt");
+
+            _watcher.Created += FileCreated;
+
+            _watcher.IncludeSubdirectories = false;
+
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            string content = File.ReadAllText(e.FullPath);
+            string upperCase = content.ToUpperInvariant();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertFileName = Path.GetFileName(e.FullPath) + ".converted";
+            var convertedPath = Path.Combine(dir, convertFileName);
+            File.WriteAllText(convertedPath, upperCase);
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+            return true;
+        }
+    }
+}
---
+++ Services/FileConverterService/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace FileConverterService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(
+                        () => new ConverterService());
+
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(stop => stop.Stop());
+                });
+                serviceConfig.SetServiceName("AwesomeFileConverter");
+                serviceConfig.SetDisplayName("Awesome File Converter");
+                serviceConfig.SetDescription("A pluralsight demo service!");
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
